761069c9250566f5841987ea155f06f2deb31247
Ashish Gaikwad <ashish@letscooee.com>
2022-09-08 16:04:35 +0530

    refactor: Use ObjectiveC notification service COOEE-1029

--- example/ios/CooeeReactNativeExample/AppDelegate.m
+++ example/ios/CooeeReactNativeExample/AppDelegate.m
@@ -20,6 +20,7 @@
 #import <FlipperKitNetworkPlugin/FlipperKitNetworkPlugin.h>
 #import <SKIOSNetworkPlugin/SKIOSNetworkAdapter.h>
 #import <FlipperKitReactPlugin/FlipperKitReactPlugin.h>
+
 static void InitializeFlipper(UIApplication *application) {
   FlipperClient *client = [FlipperClient sharedClient];
   SKDescriptorMapper *layoutDescriptorMapper = [[SKDescriptorMapper alloc] initWithDefaults];
@@ -38,72 +39,69 @@ static void InitializeFlipper(UIApplication *application) {
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions
 {
-  #ifdef FB_SONARKIT_ENABLED
-    InitializeFlipper(application);
-  #endif
+#ifdef FB_SONARKIT_ENABLED
+  InitializeFlipper(application);
+#endif
   //[AppController configure];
   [CooeePlugin appLaunched];
   RCTBridge *bridge = [[RCTBridge alloc] initWithDelegate:self launchOptions:launchOptions];
   RCTRootView *rootView = [[RCTRootView alloc] initWithBridge:bridge
                                                    moduleName:@"CooeeReactNativeExample"
                                             initialProperties:nil];
-
+  
   rootView.backgroundColor = [[UIColor alloc] initWithRed:1.0f green:1.0f blue:1.0f alpha:1];
-
+  
   self.window = [[UIWindow alloc] initWithFrame:[UIScreen mainScreen].bounds];
   UIViewController *rootViewController = [UIViewController new];
   rootViewController.view = rootView;
   self.window.rootViewController = rootViewController;
   [self.window makeKeyAndVisible];
-
+  
   UNUserNotificationCenter *center = [UNUserNotificationCenter currentNotificationCenter];
-          center.delegate = self;
-          [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
-              if( !error ) {
-                  // Once permission granted register for remote notification
-                  dispatch_async(dispatch_get_main_queue(), ^{
-                      [[UIApplication sharedApplication] registerForRemoteNotifications];
-                  });
-              } else {
-                  // Handle error thrown by permission request
-                  NSLog( @"ERROR: %@ - %@", error.localizedFailureReason, error.localizedDescription );
-              }
-          }];
-
+  center.delegate = self;
+  [center requestAuthorizationWithOptions:(UNAuthorizationOptionSound | UNAuthorizationOptionAlert | UNAuthorizationOptionBadge) completionHandler:^(BOOL granted, NSError * _Nullable error) {
+    if( !error ) {
+      // Once permission granted register for remote notification
+      dispatch_async(dispatch_get_main_queue(), ^{
+        [[UIApplication sharedApplication] registerForRemoteNotifications];
+      });
+    } else {
+      // Handle error thrown by permission request
+      NSLog( @"ERROR: %@ - %@", error.localizedFailureReason, error.localizedDescription );
+    }
+  }];
+  
   return YES;
 }
 
 - (NSURL *)sourceURLForBridge:(RCTBridge *)bridge
 {
-/*#if DEBUG
-  return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-#else*/
+  /*#if DEBUG
+   return [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+   #else*/
   return [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
-//#endif
+  //#endif
 }
 
 - (void)application:(UIApplication *)application didRegisterForRemoteNotificationsWithDeviceToken:(NSData *)deviceToken
-    {
-        // Send device token to the Cooeeprintlet tokenString = rawToken.reduce("") {
-//      $0 + String(format: "%02X", $1)
-//  }
-      NSLog(@"%@", deviceToken);
-        [CooeePlugin setDeviceTokenWithToken:deviceToken];
-    }
+{
+  NSLog(@"%@", deviceToken);
+  [CooeePlugin setDeviceTokenWithToken:deviceToken];
+}
 
-    - (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
-    {
-        // Cooee can show only In-App while app is in foreground. Here Cooee will check for there notification and
-        // decide what action should be taken on foreground notification. If Push notification do not belong to
-        // Cooee then default [.badge,.alert,.sound] is return
-        completionHandler([CooeePlugin presentNotification:notification]);
-    }
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center willPresentNotification:(UNNotification *)notification withCompletionHandler:(void (^)(UNNotificationPresentationOptions))completionHandler
+{
+  // Cooee can show only In-App while app is in foreground. Here Cooee will check for there notification and
+  // decide what action should be taken on foreground notification. If Push notification do not belong to
+  // Cooee then default [.badge,.alert,.sound] is return
+  completionHandler([CooeePlugin presentNotification:notification]);
+}
 
-    - (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
-    {
-        // Cooee will perform click action associated with Push Notification
-        [CooeePlugin notificationAction:response];
-        completionHandler();
-    }
+- (void)userNotificationCenter:(UNUserNotificationCenter *)center didReceiveNotificationResponse:(UNNotificationResponse *)response withCompletionHandler:(void (^)(void))completionHandler
+{
+  // Cooee will perform click action associated with Push Notification
+  [CooeePlugin notificationAction:response];
+  completionHandler();
+}
 
 @end
---
+++ example/ios/NotifySerice/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  NotifySerice
+//
+//  Created by Ashish Gaikwad on 07/09/22.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
